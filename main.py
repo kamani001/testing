8e4e28d21a8b03da30a27843995e07464125939e
MahekKamani <mahekkamani2001@gmail.com>
2025-09-16 22:45:00 -0300

    Add initial Python script

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A simple Python script
+"""
+
+def main():
+    print("Hello, World!")
+    print("This is a new Python file.")
+
+if __name__ == "__main__":
+    main()
